6c316252b46937f2052927dc74d554b7cb1ddbbf
Julien Robert <julien@rbrt.fr>
2025-01-31 02:52:02 +0100

    feat: add `--deamon` flag

--- main.go
+++ main.go
@@ -3,15 +3,19 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
 	"path"
+	"runtime"
 
 	"github.com/spf13/cobra"
 )
 
 var (
-	version    = "1.0.0"
-	flagConfig = "config"
-	cfgFile    string
+	version     = "1.0.0"
+	flagConfig  = "config"
+	flagDaemon  = "daemon"
+	cfgFile     string
+	runAsDaemon bool
 )
 
 func main() {
@@ -23,9 +27,24 @@ func main() {
 
 	rootCmd := &cobra.Command{
 		Use:     "servmon",
-		Short:   "Server Monitoring Tool with TUI and Alerts",
+		Short:   "KISS server monitoring tool with email alerts",
 		Version: version,
 		RunE: func(cmd *cobra.Command, args []string) error {
+			runAsDaemon, err := cmd.Flags().GetBool(flagDaemon)
+			if err != nil {
+				return fmt.Errorf("error getting flag %s: %v", flagDaemon, err)
+			}
+
+			if runAsDaemon {
+				pid, err := runAsDaemonProcess()
+				if err != nil {
+					return err
+				}
+
+				cmd.Println("Running as daemon with PID", pid)
+				return nil
+			}
+
 			cfgPath, err := cmd.Flags().GetString(flagConfig)
 			if err != nil {
 				return fmt.Errorf("error getting flag %s: %v", flagConfig, err)
@@ -59,9 +78,42 @@ func main() {
 
 	rootCmd.CompletionOptions.DisableDefaultCmd = true
 	rootCmd.PersistentFlags().StringVar(&cfgFile, flagConfig, path.Join(homeDir, ".servmon.yaml"), "config file")
+	rootCmd.PersistentFlags().BoolVarP(&runAsDaemon, flagDaemon, "d", false, "run as daemon")
 
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Fprint(os.Stderr, err)
 		os.Exit(1)
 	}
+}
+
+func runAsDaemonProcess() (int, error) {
+	if runtime.GOOS == "linux" || runtime.GOOS == "freebsd" {
+		var args []string
+		for _, a := range os.Args[1:] {
+			if a != "-d" && a != "--daemon" {
+				args = append(args, a)
+			}
+		}
+
+		cmd := exec.Command(os.Args[0], args...)
+		cmd.Stdout = os.NewFile(3, "log.out")
+		cmd.Stderr = os.NewFile(4, "log.err")
+		cmd.Stdin = os.NewFile(3, "log.in")
+
+		if err := cmd.Start(); err != nil {
+			return 0, fmt.Errorf("error starting as daemon: %v", err)
+		}
+
+		pid := cmd.Process.Pid
+
+		// Detach the process
+		err := cmd.Process.Release()
+		if err != nil {
+			return 0, fmt.Errorf("error detaching process: %v", err)
+		}
+
+		return pid, nil
+	}
+
+	return 0, fmt.Errorf("daemon mode is not supported on %s", runtime.GOOS)
 }
